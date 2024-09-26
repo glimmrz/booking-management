fd17b0b3979e84e7e76620f69abef5891694ed3f
tayef mahmud <mahmudovi183@gmail.com>
2024-09-26 13:01:09 +0600

    sidebar text style

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -15,28 +15,28 @@ export default function Sidebar() {
       <div className="flex flex-col gap-4 text-slate-500">
         <Link
           href="/dashboard/front-desk"
-          className="flex items-center justify-between px-4 py-2 hover:bg-white transition-colors duration-300"
+          className="flex items-center gap-4 px-4 py-2 hover:bg-white transition-colors duration-300"
         >
           <Icon icon="chart" size={28} />
           {isExpended && <span className="capitalize">front desk</span>}
         </Link>
         <Link
           href="/dashboard/reservation"
-          className="flex items-center justify-between px-4 py-2 hover:bg-white transition-colors duration-300"
+          className="flex items-center gap-4 px-4 py-2 hover:bg-white transition-colors duration-300"
         >
           <Icon icon="user" size={28} />
           {isExpended && <span className="capitalize">reservation</span>}
         </Link>
         <Link
           href="/dashboard/overview"
-          className="flex items-center justify-between px-4 py-2 hover:bg-white transition-colors duration-300"
+          className="flex items-center gap-4 px-4 py-2 hover:bg-white transition-colors duration-300"
         >
           <Icon icon="security" size={28} />
           {isExpended && <span className="capitalize">overview</span>}
         </Link>
         <Link
           href=""
-          className="flex items-center justify-between px-4 py-2 hover:bg-white transition-colors duration-300"
+          className="flex items-center gap-4 px-4 py-2 hover:bg-white transition-colors duration-300"
         >
           {/* <div className="text-blue-400 border border-slate-400 bg-white p-2 rounded-full"> */}
           <Icon icon="settings" size={28} />
@@ -44,7 +44,7 @@ export default function Sidebar() {
         </Link>
         <Link
           href=""
-          className="flex items-center justify-between px-4 py-2 hover:bg-white transition-colors duration-300"
+          className="flex items-center gap-4 px-4 py-2 hover:bg-white transition-colors duration-300"
         >
           <Icon icon="group" size={28} />
           {isExpended && <span className="capitalize">check-out</span>}
